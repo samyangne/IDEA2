e198b13df7490fce09c5bbae7cb103755ecb126d
samyangne <1129993231@qq.com>
2017-01-17 17:02:27 +0800

    Update Test.java

--- src/cn/tarena/Test.java
+++ src/cn/tarena/Test.java
@@ -8,6 +8,7 @@ public class Test {
         int a=10;
         for (int i=0;i<10;i++){
             System.out.println("失败乃成功之母");
+            System.out.println("i have a dream ,i want to change the world");
         }
     }
 }
