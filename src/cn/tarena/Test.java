af098f195debc53171f7e50517e53c42b9ea3f01
samyang <1129993231@qq.com>
2017-01-17 16:47:56 +0800

    this is a test

---
+++ src/cn/tarena/Test.java
@@ -0,0 +1,13 @@
+package cn.tarena;
+
+/**
+ * Created by Administrator on 2017/1/17 0017.
+ */
+public class Test {
+    public static void main(String[] args) {
+        int a=10;
+        for (int i=0;i<10;i++){
+            System.out.println("失败乃成功之母");
+        }
+    }
+}
